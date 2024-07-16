d7b9f3e2a717a2e426a6fa6a762378ea11c5d71f
zsline <sergei.dereka@gmail.com>
2024-07-16 21:39:35 +0300

    16-07-24-21

--- js/main.js
+++ js/main.js
@@ -1,10 +1,42 @@
 window.onload = function () {
   const elem = document.querySelector('.grid');
-  const iso = new Isotope(elem, {
+  const screenWidth = window.innerWidth;
+  const gridItems = document.querySelectorAll('.grid-item');
+  const gridItemFull = document.querySelectorAll('.grid-item-full');
+
+  if(screenWidth < 992 ){
+    gridItemFull.forEach(el => {
+      el.classList.remove('grid-item-full');
+    });
+    gridItems.forEach(el => {
+      el.style.position = 'relative';
+      el.style.setProperty("top", "0", "important");
+      el.style.width = '100%';
+      el.style.marginTop = '0';
+      el.classList.add('why-item__clip');
+    })
+  } else {
+    const iso = new Isotope(elem, {
     // options
     itemSelector: '.grid-item',
     layoutMode: 'masonry'
   });
+  }
+  // if(screenWidth < 992 ){
+  //   elem.classList.remove('grid');
+  //   gridItems.forEach(el => {
+  //     el.style.position = 'relative';
+  //     el.style.setProperty("top", "0", "important");
+  //     el.style.width = '100%';
+  //     el.style.marginTop = '0';
+
+  //   });
+  // }
+  // const iso = new Isotope(elem, {
+  //   // options
+  //   itemSelector: '.grid-item',
+  //   layoutMode: 'masonry'
+  // });
   
 
 };
