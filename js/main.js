b6bd668498766f37d0dc1798e024b9be2ac47727
zsline <sergei.dereka@gmail.com>
2024-07-24 20:32:43 +0300

    24-07-24-20

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 window.onload = function () {
   const elem = document.querySelector('.grid');
+  const blogElem = document.querySelector('.blog-grid');
   const screenWidth = window.innerWidth;
   const gridItems = document.querySelectorAll('.grid-item');
   const gridItemFull = document.querySelectorAll('.grid-item-full');
+  if (blogElem) {
+    const iso2 = new Isotope(blogElem, {
+      // options
+      itemSelector: '.blog__item',
+      layoutMode: 'masonry'
+    });
+  }
 
-  if(screenWidth < 992 ){
+  if (screenWidth < 992) {
     gridItemFull.forEach(el => {
       el.classList.remove('grid-item-full');
     });
@@ -17,38 +25,21 @@ window.onload = function () {
     })
   } else {
     const iso = new Isotope(elem, {
-    // options
-    itemSelector: '.grid-item',
-    layoutMode: 'masonry'
-  });
+      // options
+      itemSelector: '.grid-item',
+      layoutMode: 'masonry'
+    });
   }
-  // if(screenWidth < 992 ){
-  //   elem.classList.remove('grid');
-  //   gridItems.forEach(el => {
-  //     el.style.position = 'relative';
-  //     el.style.setProperty("top", "0", "important");
-  //     el.style.width = '100%';
-  //     el.style.marginTop = '0';
-
-  //   });
-  // }
-  // const iso = new Isotope(elem, {
-  //   // options
-  //   itemSelector: '.grid-item',
-  //   layoutMode: 'masonry'
-  // });
-  
-
 };
 
-  // ========== MENU =================
-  const openBtn = document.querySelector('.header__btn--open');
-  const closeBtn = document.querySelector('.header__btn--close');
-  const navMenu = document.querySelector('.header__nav');
+// ========== MENU =================
+const openBtn = document.querySelector('.header__btn--open');
+const closeBtn = document.querySelector('.header__btn--close');
+const navMenu = document.querySelector('.header__nav');
 
-  openBtn.addEventListener('click', () => {
-    navMenu.classList.add('open');
-  });
-  closeBtn.addEventListener('click', () =>{
-    navMenu.classList.remove('open');
-  });
+openBtn.addEventListener('click', () => {
+  navMenu.classList.add('open');
+});
+closeBtn.addEventListener('click', () => {
+  navMenu.classList.remove('open');
+});
